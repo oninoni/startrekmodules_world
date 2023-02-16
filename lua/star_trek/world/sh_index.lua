db187ce790a47762c1e2aa83922471ace3f26253
Oninoni <jan@cheap-purple.com>
2023-02-17 00:53:53 +0100

    Seperate Default Galaxy from System

--- lua/star_trek/world/sh_index.lua
+++ lua/star_trek/world/sh_index.lua
@@ -50,6 +50,7 @@ if SERVER then
 	include("net/sv_net.lua")
 	include("sv_entities.lua")
 
+	include("sv_galaxy.lua")
 	include("sv_systems.lua")
 end
 
--- lua/star_trek/world/sv_systems.lua
+++ lua/star_trek/world/sv_systems.lua
@@ -50,6 +50,11 @@ function Star_Trek.World:LoadStarSystem(leaf)
 			return false, worldEnt
 		end
 
+		local orbitOffset = entData.OrbitOffset
+		if isvector(orbitOffset) then
+			worldEnt.OrbitOffset = orbitOffset
+		end
+
 		worldEnt.Name = entData.Name
 	end
 
@@ -84,6 +89,10 @@ function Star_Trek.World:UnloadStarSystem(leaf)
 end
 
 -- Update the locations, that should currently be loaded.
+--
+-- @param Number x
+-- @param Number y
+-- @param Number r
 function Star_Trek.World:UpdateLocations(x, y, r)
 	local leafs = self.QuadTree:QueryBounds(x, y, r)
 
@@ -109,87 +118,37 @@ timer.Create("Star_Trek.World.UpdateMap", 10, 0, function()
 		local pos = ship.Pos
 
 		Star_Trek.World:UpdateLocations(pos:GetX(), pos:GetY(), LY(5))
-
-		print("D: ", math.Round(SBtoLY(Star_Trek.World.MapShip.Pos:Length()), 4))
-		print("W: ", math.Round(SBtoW(Star_Trek.World.MapShip.Vel:Length()), 2))
-		print(Star_Trek.World.MapShip.ActiveManeuver.Duration / 60)
 	end)
 end)
-
--- Adding the map ship.
--- An Intrepid class vessel, that is represented by the map.
---
--- @return Boolean success
--- @return String error
-function Star_Trek.World:AddMapShip()
-	local dir = Angle(0, math.random(0, 360), 0)
-
-	local _, station = Star_Trek.World:LoadEntity(100, "base", self.Entities[4]:GetStandardOrbit(), dir, "models/crazycanadian/star_trek/stations/spacedock.mdl", KM(5))
-	station.OrbitOffset = Vector(0, 0, KM(4.2))
-	self.Stattion = station
-
-	local _, mapShip = Star_Trek.World:LoadEntity(1, "ship", station:GetStandardOrbit(), Angle(), "models/kingpommes/startrek/intrepid/intrepid_sky_1024.mdl")
-	self.MapShip = mapShip
-
-	for i = 1, 1 do
-		local pos = LocalToWorldBig(Vector(0, 0, 0), Angle(), mapShip.Pos, mapShip.Ang)
-		local _, block = Star_Trek.World:LoadEntity(100 + i, "ship", pos, ang, "models/hunter/blocks/cube4x4x4.mdl", M(50))
-		block:SetVelocity(Vector(-1, 1, -1))
-	end
-end
 
 -- Setup the galaxy.
 function Star_Trek.World:ReLoadGalaxy()
 	local loadedLeafs = table.Copy(self.LoadedLeafs or {})
 	for _, leaf in pairs(loadedLeafs) do
-		Star_Trek.World:UnloadStarSystem(leaf)
+		self:UnloadStarSystem(leaf)
 	end
 	self.LoadedLeafs = {}
 
-	self.QuadTree = QuadTree(0, 0, LY(Star_Trek.World.MaxDistance))
+	self.QuadTree = QuadTree(0, 0, LY(self.MaxDistance))
 	local override = hook.Run("Star_Trek.World.LoadGalaxy", self.QuadTree)
 	if override then return end
 
-	local solLeaf = self.QuadTree:CreateLeaf(LY(0), LY(0), {Name = "Sol System", Entities = {
-		{Id = 11, OrbitRadius = AU(0), Name = "Sol", Class = "sun", Model = {Model = "models/crazycanadian/star_trek/planets/star.mdl", Skin = 2}, Diameter = KM(1392700), LightColor = Vector(1.3, 1.2, 1)},
-		{Id = 2, OrbitRadius = AU(0.39), Name = "Mercury", Class = "planet", Model = "models/planets/mercury.mdl", Diameter = KM(4880)},
-		{Id = 3, OrbitRadius = AU(0.72), Name = "Venus", Class = "planet", Model = "models/planets/venus.mdl", Diameter = KM(12104)},
-		{Id = 4, OrbitRadius = AU(1), Name = "Earth", Class = "planet", Model = nil, Diameter = KM(12142)},
-		{Id = 5, OrbitRadius = AU(1.52), Name = "Mars", Class = "planet", Model = "models/planets/mars.mdl", Diameter = KM(6780)},
-		{Id = 6, OrbitRadius = AU(5.20), Name = "Jupiter", Class = "planet", Model = "models/planets/jupiter.mdl", Diameter = KM(139822)},
-		{Id = 7, OrbitRadius = AU(9.58), Name = "Saturn", Class = "planet", Model = "models/planets/saturn.mdl", Diameter = KM(116464)},
-		{Id = 8, OrbitRadius = AU(19.20), Name = "Uranus", Class = "planet", Model = "models/planets/uranus.mdl", Diameter = KM(50724)},
-		{Id = 9, OrbitRadius = AU(30.05), Name = "Neptun", Class = "planet", Model = "models/planets/neptune.mdl", Diameter = KM(29244)},
-
-		-- Earth Orbit
-		{Id = 10, ParentId = 4, OrbitRadius = KM(385000), Name = "Luna", Class = "planet", Model = "models/planets/luna_big.mdl", Diameter = KM(3474.8)},
-	}})
-	self.QuadTree:CreateLeaf(LY(-0.6162192048), LY(-12.8379001), {Name = "Vulcan System", Entities = {
-		{Id = 12, OrbitRadius = AU(0), Name = "40 Eridiani A", Class = "sun", Diameter = KM(1130872.4), LightColor = Vector(1, 1, 1)},
-		{Id = 13, OrbitRadius = AU(0.3), Name = "40 Eridiani B", Class = "sun", Diameter = KM(19497.8), LightColor = Vector(1, 1, 1)},
-		{Id = 14, OrbitRadius = AU(0.2), Name = "40 Eridiani C", Class = "sun", Diameter = KM(431737), LightColor = Vector(1, 1, 1)},
-
-		{Id = 15, OrbitRadius = AU(0.9), Name = "Vulcan", Class = "planet", Model = "models/planets/mars.mdl", Diameter = KM(12142)},
-	}})
-	self.QuadTree:CreateLeaf(LY(6.264895249), LY(-9.756804077), {Name = "Andoria System", Entities = {
-		{Id = 16, OrbitRadius = AU(0), Name = "Procyon A", Class = "sun", Diameter = KM(2855035), LightColor = Vector(1, 1, 1)},
-		{Id = 17, OrbitRadius = AU(0.3), Name = "Procyon B", Class = "sun", Diameter = KM(16712.4), LightColor = Vector(1, 1, 1)},
-
-		{Id = 18, OrbitRadius = AU(2), Name = "Andor", Class = "planet", Model = "models/planets/earth.mdl", Diameter = KM(12142)},
-	}})
-	self.QuadTree:CreateLeaf(LY(-11.86221969), LY(1.386493211), {Name = "Tellar System", Entities = {
-		{Id = 19, OrbitRadius = AU(0), Name = "61 Cygni A", Class = "sun", Diameter = KM(926145.5), LightColor = Vector(1, 1, 1)},
-		{Id = 20, OrbitRadius = AU(0.2), Name = "61 Cygni B", Class = "sun", Diameter = KM(828656.5), LightColor = Vector(1, 1, 1)},
-
-		{Id = 21, OrbitRadius = AU(1.5), Name = "Tellar", Class = "planet", Model = "models/planets/earth.mdl", Diameter = KM(12142)},
-	}})
-
-	Star_Trek.World:LoadStarSystem(solLeaf)
-	Star_Trek.World:AddMapShip()
+	self:LoadDefaultGalaxy()
 end
 
 hook.Add("InitPostEntity", "Star_Trek.World.LoadGalaxy", function() Star_Trek.World:ReLoadGalaxy() end)
 hook.Add("PostCleanupMap", "Star_Trek.World.LoadGalaxy", function() Star_Trek.World:ReLoadGalaxy() end)
+
+
+
+
+
+
+
+
+
+
+
 
 function flyToVulcan()
 	local ship = Star_Trek.World.Entities[1]
